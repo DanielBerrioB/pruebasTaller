5050c86be59fc00758c7815b9512b3dc31e8234c
DaaGeney <46972313+DaaGeney@users.noreply.github.com>
2020-09-30 19:19:22 -0500

    function

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function main() {
   function1();
-  function2();
+  console.log(function2());
   function3();
+
 }
 
 function function1() {}
 
-function function2() {}
+function2 = (n) =>{
+  return n <= 1 ? "error" : (n + 4)/Math.sqrt(n-1)
+}
 
 function function3() {}
-
 main();
