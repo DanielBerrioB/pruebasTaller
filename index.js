8274d9cbb6d7a774f211cc784af1b54ed7d7160f
celciusjj <juanjjo99@gmail.com>
2020-09-30 19:19:18 -0500

    Function3

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function main() {
   function1();
   function2();
-  function3();
+  function3(0);
 }
 
-function function1() {}
+function function1() { }
 
-function function2() {}
+function function2() { }
 
-function function3() {}
-
+/**
+ * Calcula el logaritmo de un Numero
+ */
+function3 = (number) => {
+  return number <= 0 ? "La funcion tiene dominio es restringido"
+    :
+    number == undefined ? "Ingrese un parametro valido"
+      :
+      Math.log(number);
+}
 main();
