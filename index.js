623bb587bd0fb8bae378d4a83ebe97d451670e32
DanielBerrioB <daniberrio10@hotmail.com>
2020-10-03 20:50:28 -0500

    Add more tests to cover additional cases

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 function main(option, num) {
   if (option === 1) {
-    return function1(num);
-  } else if (option === 2) {
-    return function2();
-  } else if (option === 3) {
-    return function3();
+    return rationalFunction(num);
+  //More options can be added
   } else {
     return "Opción inválida";
   }
 }
 
-function function1(num) {
+function rationalFunction(num) {
   return 1 / (num - Math.pow(num, 2) + 2);
 }
-
-function function2() {}
-
-function function3() {}
 
 module.exports = { main };
--- index.test.js
+++ index.test.js
@@ -1,13 +1,17 @@
 const functions = require("./index");
 
 describe("Tests", () => {
-  it("Function 1, test ideal", () => {
+  it("rationalFunction, test ideal", () => {
     expect(functions.main(1, 1)).toBe(0.5);
   });
-  it("Function 1, Infinity test when the functions is out of the domain", () => {
+  it("rationalFunction, Infinity test when the functions is out of the domain", () => {
     expect(functions.main(1, 2)).toBe(Infinity);
   });
-  it("Function 1, Infinity test when the functions is out of the domain", () => {
+  it("rationalFunction, when the X point is 0 or less that it", () => {
+    expect(functions.main(1, -2)).toBe(-0.25);
+    expect(functions.main(1, 0)).toBe(0.5);
+  });
+  it("rationalFunction, tests if the options does not match", () => {
     expect(functions.main(4, 2)).toBe("Opción inválida");
   });
 });
